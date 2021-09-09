48be134eddacd0e89e7694a1bc4d8a1d32c70811
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-09-09 10:38:53 +0100

    Created a dependency injection

---
+++ CustomerApp_RefactorStarter/NorthwindData/Services/CustomerService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindData.Services
+{
+    class CustomerService : ICustomerServices
+    {
+        private readonly NorthwindContext _context;
+
+        // Use dependency injection !
+
+        public CustomerService(NorthwindContext context)
+        {
+            _context = context;
+        }
+        public CustomerService()
+        {
+            _context = new NorthwindContext();
+        }
+
+        public void CreateCustomer(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomerById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetCustomerList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCustomer(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveCustomerChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomerApp_RefactorStarter/NorthwindData/Services/ICustomerServices.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace NorthwindData.Services
+{
+    public interface ICustomerServices
+    {
+        List<Customer> GetCustomerList();
+        public Customer GetCustomerById();
+        public void CreateCustomer(Customer c);
+        public void SaveCustomerChanges();
+        public void RemoveCustomer(Customer c);
+    }
+}
